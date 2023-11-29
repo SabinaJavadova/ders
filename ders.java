35a4d1c11ffabfcef6df66b617cb882e98abcfd3
Sabina Javadova <152090079+SabinaJavadova@users.noreply.github.com>
2023-11-29 15:42:30 +0400

    Add files via upload

---
+++ ders.java
@@ -0,0 +1,57 @@
+package derrs;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	///////primitiv tipler///////
+			int x =123;
+			System.out.println(x);
+			
+			float y = 2.35f;
+			System.out.println(y);
+			
+			boolean z = true;
+			System.out.println(z);
+			
+			char symbol = 's';
+			System.out.println(symbol);
+			
+			String name = "sabina";
+			System.out.println(name);
+			
+			
+			///////operators//////
+			
+			int sum = 100 + 24;
+			System.out.println(sum);
+			
+		
+			int a = 10;
+			int b = 5;
+			System.out.println(a-b);
+			
+			
+			int a1 = 2;
+			int b1 = 4;
+			System.out.println(a1*b1);
+			
+			int m = 1240;
+			int n = 10;
+			System.out.println(m/n);
+			
+			int eded1 = 13;
+			int eded2 = 5;
+			System.out.println(eded1%eded2);
+			
+			int number = 7;
+			++number;
+			System.out.println(number);
+			
+			int c = 10;
+			--c;
+			System.out.println(c);
+					
+	}
+
+}
