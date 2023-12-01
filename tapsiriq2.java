58f7657c5b2458c2bae995432724ade129fadc23
Sabina Javadova <152090079+SabinaJavadova@users.noreply.github.com>
2023-12-02 02:39:19 +0400

    Create tapsiriq2.java
    ikinci tapsrq

---
+++ tapsiriq2.java
@@ -0,0 +1,22 @@
+//1ci sual//
+String aString = "salam";
+		String bString = "sagol";
+		
+		if (aString.length() > bString.length()) {
+			System.out.println( "The first string is longer."); 
+			} else if (aString.length() < bString.length()) {
+			System.out.println("The second string is longer.");
+			} else if (aString.length() == bString.length()) {
+			System.out.println("Both strings are of equal length.");
+			} else {
+				System.out.println("sorgu yanlistir.");
+			}
+
+//2ci sual//
+
+	int cem = 0;
+		for ( int i = 1 ;i <= 10; i++) {
+			cem +=i;
+		} System.out.println( cem);
+//3cu sual//
+-
